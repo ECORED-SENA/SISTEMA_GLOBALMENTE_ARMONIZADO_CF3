f2328a552ad096ac3a807daaf3dfacc55f69d81f
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2021-05-31 16:51:48 -0500

    Avance 6
    Se cargan videos y se modifica el material descargable

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -125,7 +125,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/fk298284tQo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     #t_1_4.titulo-segundo
       .h4 1.4 Ingreso y salida de productos químicos
@@ -255,7 +255,7 @@
 
       figure.mb-5
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/dO16cgzq3RQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       
       .h5.mb-4 1.7.3. Espacios
 
